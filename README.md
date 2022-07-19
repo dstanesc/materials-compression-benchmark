9e350e475b89e62047b510a8731c81a6ffa5423d
Dragos Stanescu <dstanesc@gmail.com>
2022-07-19 14:23:54 -0400

    Initial deaft

---
+++ README.md
@@ -0,0 +1,74 @@
+# Material Data Compression Benchmark
+
+Suite of material data compression benchmarks based on the [fake material data](https://github.com/dstanesc/fake-material-data) library.
+
+Currently tested libraries: `brotli`, `pako` and `lz4js`.
+
+## Dependencies
+
+```sh
+$ node --version
+v16.13.1
+
+$ npm --version
+8.1.2
+```
+
+## Execute Benchmark
+
+```sh
+npm run clean
+npm install
+npm start
+```
+
+## Example Results
+
+Eg. Material w/ 100 properties
+```
+Material original size 53398 bytes
+Material brotli compressed size 6675 bytes, compression rate 87.50 %
+Material pako compressed size 6893 bytes, compression rate 87.09 %
+Material lz4 compressed size 8288 bytes, compression rate 84.48 %
+Brotli x 1,293 ops/sec ±0.46% (94 runs sampled)
+Pako x 1,304 ops/sec ±0.52% (91 runs sampled)
+Lz4js x 3,311 ops/sec ±0.59% (89 runs sampled)
+The fastest option is Lz4js
+Material brotli compressed size 5811 bytes, compression rate 89.12 %
+Material pako compressed size 6471 bytes, compression rate 87.88 %
+Material lz4 compressed size 8288 bytes, compression rate 84.48 %
+Brotli x 601 ops/sec ±0.86% (91 runs sampled)
+Pako x 730 ops/sec ±0.86% (88 runs sampled)
+Lz4js x 3,334 ops/sec ±0.54% (90 runs sampled)
+The fastest option is Lz4js
+Material brotli compressed size 5192 bytes, compression rate 90.28 %
+Material pako compressed size 6339 bytes, compression rate 88.13 %
+Material lz4 compressed size 8288 bytes, compression rate 84.48 %
+Brotli x 7.05 ops/sec ±1.21% (22 runs sampled)
+Pako x 632 ops/sec ±1.49% (87 runs sampled)
+Lz4js x 3,253 ops/sec ±2.06% (89 runs sampled)
+The fastest option is Lz4js
+```
+
+```json
+{
+  min: {
+    bufSize: 53398,
+    ops: { brotli: 1293, pako: 1303, lz4js: 3310 },
+    rate: { brotli: '87.50', pako: '87.09', lz4js: '84.48' },
+    options: { brotli: 1, pako: 1 }
+  },
+  med: {
+    bufSize: 53398,
+    ops: { brotli: 601, pako: 729, lz4js: 3334 },
+    rate: { brotli: '89.12', pako: '87.88', lz4js: '84.48' },
+    options: { brotli: 5, pako: 5 }
+  },
+  max: {
+    bufSize: 53398,
+    ops: { brotli: 7, pako: 631, lz4js: 3253 },
+    rate: { brotli: '90.28', pako: '88.13', lz4js: '84.48' },
+    options: { brotli: 11, pako: 9 }
+  }
+}
+```
---
+++ benchmark.js
@@ -0,0 +1,116 @@
+import Benchmark from 'benchmark'
+
+import {
+  simpleMeta,
+  simpleMaterialJson,
+} from '@dstanesc/fake-material-data'
+
+import { pack } from 'msgpackr';
+
+import * as lz4 from 'lz4js'
+import * as pako from 'pako'
+import * as brotli from 'brotli'
+
+const rate = (origSize, deflatedSize) => {
+  return (((origSize - deflatedSize) / origSize) * 100).toFixed(2);
+}
+
+const meta = simpleMeta([4, 500, 100]);
+
+const matData = (args) => {
+  const meta = simpleMeta(args);
+  const matJson = simpleMaterialJson(meta);
+  const buf = pack(matJson);
+  const bufSize = buf.byteLength;
+  console.log(`Material original size ${bufSize} bytes`);
+  return { buf, bufSize }
+}
+
+const bench = ({ buf, bufSize }, options) => {
+  const serPako = pako.deflate(buf, options.pako);
+  const serLz4 = lz4.compress(buf);
+  const serBrotli = brotli.compress(buf, options.brotli);
+
+  const serPakoSize = serPako.byteLength;
+  const serLz4Size = serLz4.byteLength;
+  const serBrotliSize = serBrotli.byteLength;
+
+  const brotliRate = rate(bufSize, serBrotliSize);
+  const pakoRate = rate(bufSize, serPakoSize);
+  const lz4jsRate = rate(bufSize, serLz4Size);
+
+  console.log(`Material brotli compressed size ${serBrotliSize} bytes, compression rate ${brotliRate} %`);
+  console.log(`Material pako compressed size ${serPakoSize} bytes, compression rate ${pakoRate} %`);
+  console.log(`Material lz4 compressed size ${serLz4Size} bytes, compression rate ${lz4jsRate} %`);
+
+  const compressSuite = new Benchmark.Suite('Material 300K Compression Suite')
+
+  compressSuite.on('complete', event => {
+    const suite = event.currentTarget;
+    const fastestOption = suite.filter('fastest').map('name')
+    console.log(`The fastest option is ${fastestOption}`)
+  })
+
+  let brotliHz;
+  let pakoHz;
+  let lz4jsHz;
+
+  compressSuite.on('cycle', event => {
+    const benchmark = event.target;
+    console.log(benchmark.toString());
+    switch (benchmark.name) {
+      case 'Brotli':
+        brotliHz = Math.floor(benchmark.hz);
+        break;
+      case 'Pako':
+        pakoHz = Math.floor(benchmark.hz);
+        break;
+      case 'Lz4js':
+        lz4jsHz = Math.floor(benchmark.hz);
+        break;
+
+    }
+  });
+
+  compressSuite
+    .add('Brotli', async () => {
+      brotli.compress(buf, options.brotli);
+    })
+    .add('Pako', async () => {
+      pako.deflate(buf, options.pako);
+    })
+    .add('Lz4js', async () => {
+      lz4.compress(buf);
+    })
+    .run()
+
+  return { bufSize: bufSize, ops: { brotli: brotliHz, pako: pakoHz, lz4js: lz4jsHz }, rate: { brotli: brotliRate, pako: pakoRate, lz4js: lz4jsRate }, options: { brotli: options.brotli.quality, pako: options.pako.level } }
+}
+
+const mat20 = matData([4, 20, 100]); // 20 props ~ 10KB
+const mat100 = matData([4, 100, 100]); // 100 props ~ 50KB
+const mat500 = matData([4, 500, 100]); // 500 props ~ 250KB
+const mat1000 = matData([4, 1000, 100]); // 1000 props ~ 500KB
+
+const qualityBench = (matData) => {
+  const min = bench(matData, { brotli: { quality: 1 }, pako: { level: 1 } });  // min compression
+  const med = bench(matData, { brotli: { quality: 5 }, pako: { level: 5 } });  // medium compression
+  const max = bench(matData, { brotli: { quality: 11 }, pako: { level: 9 } }); // max compression
+  return { min, med, max }
+}
+
+const res20 = qualityBench(mat20);
+
+console.log(res20)
+
+const res100 = qualityBench(mat100);
+
+console.log(res100)
+
+const res500 = qualityBench(mat500);
+
+console.log(res500)
+
+const res1000 = qualityBench(mat1000);
+
+console.log(res1000)
